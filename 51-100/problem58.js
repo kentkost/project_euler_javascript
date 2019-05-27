1e9d3b3b35afe4534e1c3d4c5ff6951d45a96cb1
Kent Kostelac <kent_kostelac@hotmail.com>
2019-05-27 13:22:00 +0200

    VScode did fuckery on me

--- 51-100/problem58.js
+++ 51-100/problem58.js
@@ -1,40 +1,40 @@
-var aks = require("./aks-primality-check.js");
-
 var start=3;
 var steps =2;
-var numbers = [1]
-var primes = []
+var numbers = [];
+var numPrimes = 0;
+var numNumbers = 1;
 function addNewLayer(){
     let num = start;
     numbers.push(num);
     for(let i=0; i<2; i++){
-        num += steps;
+        num = num + steps;
         numbers.push(num);
     }
-    steps++;
+    steps+=1;
     start = num+(steps*2);
-    steps++;
+    steps+=1;
+    return numbers;
 }
 
 function ratioOfPrimes(){
-    let len =numbers.length+((numbers.length-1)/3);
-    let numPrimes = 0.0;
+    // console.log(numbers)
     numbers.forEach(function(v1,v2, set){
+        numNumbers++;
         if(isPrime(v1)){
             numPrimes++;
         }
     });
-    return numPrimes/len*100;
+    numbers.splice(0,numbers.length)
+    numNumbers++;
+    return numPrimes/numNumbers*100;
 }
 
 //miller+rabin
-const isPrime = (num) => {
-	if (!Number.isInteger(num))
-		return false;
-
+const isPrime = (n) => {
+    let num = n;
 	// Prime numbers are whole, non-negative numbers
 	// 1 is a composite number (not prime as it's only divisible by itself)
-	if (num < 2)
+    if (num<2)
 		return false;
 
 	// 2 is the first prime number
@@ -43,11 +43,11 @@ const isPrime = (num) => {
 
 	// Excluding 2 no even number can be prime
 	if (num % 2 === 0)
-		return false;
-
+        return false;
+        
 	// Run divisibility tests up to and including it's square root
-	const sqrt = Math.sqrt(num);
-	for (let i = 3; i <= sqrt; i++) {
+    const sqrt = Math.sqrt(num);
+    for (let i = 3; i < sqrt; i++) {
 		if(num % i === 0)
 			return false;
 	}
@@ -57,27 +57,23 @@ const isPrime = (num) => {
 
 //4175
 //15861 with primetester...
+//ratio: 10.23600376138162 with less console output and big number.
+//honestly I think it is because I output too much... VScode
+//removed big number: Ratio again: 10.23600376138162
 function solution(){
-    // let testprime = 17443153;
-    // console.log(primalityChecker(1000000033))
-    // console.log(9007199254740991);
-    // console.log(Number.MAX_SAFE_INTEGER) 
-    //9007199254740991
-    //29208621, arraySize=6667
-    for(let layer =0; layer<300000; layer+=1){
+    for(let layer =0; layer<30000; layer+=1){
         addNewLayer();
-        console.log((layer+1)*4+1)
-        console.log("len: "+(numbers.length+((numbers.length-1)/3)));
+        // console.log("layer: "+(layer+1)*4+1) //amount of layers total
         let ratio = ratioOfPrimes();
-        console.log("len: "+(numbers.length+((numbers.length-1)/3))+" ratio: "+ratio);
+        // console.log("primes: "+numPrimes+" numbers: "+numNumbers+" ratio: "+ratio);
+        // console.log(ratio);
         if(ratio < 10.0){
-            console.log(numbers[numbers.length-1])
-            console.log((layer*2)+1);
+            console.log((layer*2)+3); //Because we start in zero. And 1 is a lyer on its own
             break;
         }
-        console.log("greatest number: "+ numbers[numbers.length-1])
-        console.log("array size: "+numbers.length);
     }
+    console.log("program end");
+    return 1;
 }
 
 solution()
